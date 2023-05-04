2bc8ffa7458e04c443e3bea6a0a6ff3e5b15f3be
AndreiPerevoshchikov <ap300304@yandex.ru>
2023-05-04 19:18:13 +0300

    Done

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
-import flask
+#  Перевощиков Андрей 367472. Вариант 2
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/test_db7'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///phonebook.db'
 db = SQLAlchemy(app)
 
 
-class Message(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(512), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
 
-    def __init__(self, text, tags):
-        self.text = text
-        self.tags = [
-            Tag(text=tag) for tag in tags.split(',')
-        ]
+    def __repr__(self):
+        return f'<Contact {self.name}>'
 
 
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(32), nullable=False)
-
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
-    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return redirect(url_for('index'))
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    return flask.render_template('index.html', messages=Message.query.all())
+@app.route('/add', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        contact = Contact(name=name, phone=phone)
+        db.session.add(contact)
+        db.session.commit()
+    contacts = Contact.query.all()
+    return render_template('index.html', contacts=contacts)
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = flask.request.form['text']
-    tag = flask.request.form['tag']
-    # messages.append(Message(text, tag))
-    db.session.add(Message(text, tag))
+@app.route('/clear')
+def clear():
+    db.session.query(Contact).delete()
     db.session.commit()
-
-    return flask.redirect(flask.url_for('hello'))
-
-
+    return '', 204
 
 
 with app.app_context():
     db.create_all()
+
 app.run()
